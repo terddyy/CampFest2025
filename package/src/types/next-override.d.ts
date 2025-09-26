8001660f13c80b8f3df5193384cf60bd1e09eeff
terddyy <terddy03@gmail.com>
2025-09-27 07:08:28 +0800

    aw

--- package/src/types/next-override.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'next' {
-  export interface PageProps {
-    params: Promise<{ [key: string]: string | string[] }>;
-    searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
-  }
-}
