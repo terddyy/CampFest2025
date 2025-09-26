05c1c12ebc534f53b971708d1f83177dcaf2d245
terddyy <terddy03@gmail.com>
2025-09-27 05:59:23 +0800

    phase 7

--- package/src/types/next-override.d.ts
+++ package/src/types/next-override.d.ts
@@ -1,6 +1,6 @@
 declare module 'next' {
   export interface PageProps {
-    params: { [key: string]: string | string[] };
-    searchParams?: { [key: string]: string | string[] | undefined };
+    params: Promise<{ [key: string]: string | string[] }>;
+    searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
   }
 }
